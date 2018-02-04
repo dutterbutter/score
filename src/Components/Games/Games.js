08513e216eaeaad0cf4b0b32a100303becd87f99
Dustin Brickwood <dustinbrickwood204@gmail.com>
2018-02-04 01:19:07 -0500

    details pg 2morrow

--- src/Components/Games/Games.js
+++ src/Components/Games/Games.js
@@ -3,11 +3,10 @@ import './Games.css';
 
 const gameDay = (props) => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col s12">
+        
+                <div className="col s12 m4 l4">
                     <div className="card white">
-                        <div className="card-content black-text">
+                        <div className="card-content">
                             <div className="row">
                                 <div className="homeTeam col s6">
                                     <div className="row">
@@ -15,27 +14,27 @@ const gameDay = (props) => {
                                             <p className="left-align"> {props.home_team_city} </p>
                                             <p className="left-align"> {props.home_team} </p>
                                         </div>
-                                        <p className="left-align col s6"> {props.home_score} </p>
+                                        <p className="left-align col s6 score"> {props.home_score} </p>
                                     </div>
                                 </div>
                                 <div className="awayTeam col s6">
                                     <div className="row">
+                                            <p className="right-align col s6 score"> {props.away_score} </p>
                                         <div className="away col s6">
-                                            <p className="right-align"> {props.away_score} </p>
-                                        </div>
                                         <p className="right-align"> {props.away_team_city} </p>
                                         <p className="right-align"> {props.away_team} </p>
+                                        </div>
                                     </div>
                                 </div>
-                                <p className="center-align"> {props.status} </p>
-                                <p className="center-align">W: {props.winning_pitcher_last}({props.winning_pitcher_era})</p>
-                                <p className="center-align">L: {props.losing_pitcher_last}({props.losing_pitcher_era})</p>
+                                <p className="center-align status"> {props.status} </p>
+                                <p className="center-align">W: {props.winning_pitcher_last} ({props.winning_pitcher_era})</p>
+                                <p className="center-align">L: {props.losing_pitcher_last} ({props.losing_pitcher_era})</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+           
+    
     )
 }
 
--- src/Containers/GameData.js
+++ src/Containers/GameData.js
@@ -5,73 +5,77 @@ import Datepicker from '../Components/Datepicker';
 import GamesTest from '../Components/Games/Games';
 
 class GameData extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      gameData: [],
-      favoriteTeam: "",
-      isGameArray: true,
-      isEmpty: false
+    constructor() {
+        super()
+        this.state = {
+            gameData: [],
+            favoriteTeam: "",
+            isGameArray: true,
+            isEmpty: false
+        }
     }
-  }
 
-  componentDidMount() {
-    axios.get('http://localhost:8080/')
-      .then(result => {
-        this.setState({
-          gameData: result.data.games.game
+    componentDidMount() {
+        axios.get('http://localhost:8080/')
+            .then(result => {
+                this.setState({
+                    gameData: result.data.games.game
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    dateParserHandler = (selectedDate) => {
+        let dayDigit = selectedDate.slice(3, 5)
+        let month = selectedDate.slice(0, 2)
+        let year = selectedDate.slice(6, 10)
+
+        this.gameDayHandler(dayDigit, month, year)
+    }
+
+    gameDayHandler = (day, month, year) => {
+        axios.get(`http://localhost:8080/${day}${month}${year}`)
+            .then(result => {
+                this.setState({
+                    gameData: result.data.games.game
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    render() {
+
+        const games = this.state.gameData.map(game => {
+            return <Games key={game.id}
+                home_team_city={game.home_team_city}
+                home_team={game.home_team_name}
+                away_team_city={game.away_team_city}
+                away_team={game.away_team_name}
+                status={game.status.status}
+                home_score={game.linescore.r.home}
+                away_score={game.linescore.r.away}
+                winning_pitcher_last={game.winning_pitcher.last}
+                losing_pitcher_name={game.losing_pitcher.last}
+                winning_pitcher_era={game.winning_pitcher.era}
+                losing_pitcher_era={game.losing_pitcher.era} />
         })
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
 
-  dateParserHandler = (selectedDate) => {
-    let dayDigit = selectedDate.slice(3, 5)
-    let month = selectedDate.slice(0, 2)
-    let year = selectedDate.slice(6, 10)
-
-    this.gameDayHandler(dayDigit, month, year)
-  }
-
-  gameDayHandler = (day, month, year) => {
-    axios.get(`http://localhost:8080/${day}${month}${year}`)
-      .then(result => {
-        this.setState({
-          gameData: result.data.games.game
-        })
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-
-  render() {
-
-    const games = this.state.gameData.map(game => {
-      return <Games key={game.id}
-        home_team_city={game.home_team_city}
-        home_team={game.home_team_name}
-        away_team_city={game.away_team_city}
-        away_team={game.away_team_name}
-        status={game.status.status}
-        home_score={game.linescore.r.home}
-        away_score={game.linescore.r.away}
-        winning_pitcher_last = {game.winning_pitcher.last}
-        losing_pitcher_last = {game.losing_pitcher.last}
-        winning_pitcher_era = {game.winning_pitcher.era}
-        losing_pitcher_era = {game.losing_pitcher.era} />
-    })
-
-    return (
-        <div>
-          <Datepicker
-            dateParserHandler={this.dateParserHandler} />
-          {games}
-        </div>
-    );
-  }
+        return (
+            <div>
+                <Datepicker
+                    dateParserHandler={this.dateParserHandler} />
+                <div className="container">
+                    <div className="row">
+                        {games}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default GameData;
